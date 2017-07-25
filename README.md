b6c428111a0cb7a3b9dc6fc25f6042eab5db3f4a
localhost <201451051@iiitvadodara.ac.in>
2017-07-25 22:57:55 +0530

    Initial Version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Memorize
+
+## A simple and useful notes app!!
---
+++ js/retain.js
@@ -0,0 +1,82 @@
+$(function(){
+
+    var model = {
+        init: function() {
+            if (!localStorage.notes) {
+                localStorage.notes = JSON.stringify([]);
+            }
+        },
+        add: function(obj) {
+            var data = JSON.parse(localStorage.notes);
+            data.push(obj);
+            localStorage.notes = JSON.stringify(data);
+        },
+        getAllNotes: function() {
+            return JSON.parse(localStorage.notes);
+        }
+    };
+
+
+    var octopus = {
+        date: function() {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        }
+        if(mm<10){
+            mm='0'+mm;
+        }
+        var today = dd+'/'+mm+'/'+yyyy;
+        return today;
+
+         },
+
+
+        addNewNote: function(noteStr) {
+            model.add({
+                content: noteStr,
+                date: octopus.date()
+            });
+            view.render();
+        },
+
+        getNotes: function() {
+            return model.getAllNotes().reverse();
+        },
+
+        init: function() {
+            model.init();
+            view.init();
+        }
+    };
+
+
+    var view = {
+        init: function() {
+            this.noteList = $('#notes');
+            var newNoteForm = $('#new-note-form');
+            var newNoteContent = $('#new-note-content');
+            newNoteForm.submit(function(e){
+                octopus.addNewNote(newNoteContent.val());
+                newNoteContent.val('');
+                e.preventDefault();
+            });
+            view.render();
+        },
+        render: function(){
+            var htmlStr = '';
+            octopus.getNotes().forEach(function(note){
+                htmlStr += '<li class="note">'+
+                        note.content + '<p class="note-date">' + note.date + '</p>'
+                    '</li>';
+            });
+            this.noteList.html( htmlStr );
+        }
+    };
+
+    octopus.init();
+});
